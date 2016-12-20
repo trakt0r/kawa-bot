5beee4410edda587b80cc95d7716f5f53ca8afa7
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2016-12-20 22:44:24 +0100

    [#2] Prepare a working stub with minimal or no functionality

---
+++ src/KawaBot.Server/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace KawaBot.Server.Controllers
+{
+    public class HealthController
+    {
+        public static void Register(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapGet("health", ctx => ctx.Response.WriteAsync("OK"));
+        }
+    }
+}
---
+++ src/KawaBot.Server/Startup.cs
@@ -0,0 +1,33 @@
+﻿using KawaBot.Server.Controllers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace KawaBot.Server
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var routeBuilder = new RouteBuilder(app);
+            HealthController.Register(routeBuilder);
+            app.UseRouter(routeBuilder.Build());
+
+            app.Run(async ctx => ctx.Response.Redirect("/health"));
+        }
+    }
+}
