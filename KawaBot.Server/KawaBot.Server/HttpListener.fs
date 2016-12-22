c31466a15051f6fafb6a03ccad79047fe05dd126
Igor Kustov <akim13.asket@gmail.com>
2016-12-23 00:29:21 +0100

    [#2] Prepare a working stub with minimal or no functionality (#8)

---
+++ KawaBot.Server/KawaBot.Server/HttpListener.fs
@@ -0,0 +1,17 @@
+﻿namespace KawaBot.Server
+
+open System.Net
+
+module Http =
+    type HttpListener with
+        static member Run (url: string, handler: (HttpListenerRequest -> HttpListenerResponse -> Async<unit>)) = 
+            let listener = new HttpListener()
+            listener.Prefixes.Add url
+            listener.Start()
+            let asyncTask = Async.FromBeginEnd(listener.BeginGetContext, listener.EndGetContext)
+            async {
+                while true do 
+                    let! context = asyncTask
+                    Async.Start (handler context.Request context.Response)
+            } |> Async.Start 
+            listener
---
+++ KawaBot.Server/KawaBot.Server/Program.fs
@@ -0,0 +1,25 @@
+﻿namespace KawaBot.Server
+
+open System
+open System.Net
+open Utils
+open Http
+
+module Program =
+    [<EntryPoint>]
+    let main argv = 
+        printfn "[%s] Listening to the http://localhost:80/ started" (Utils.ToDateTimeString DateTime.Now)
+
+        HttpListener.Run("http://*:80/Health/", (fun req resp -> 
+                async {
+                    printfn "[%s] Received request to the /Health" (Utils.ToDateTimeString DateTime.Now)
+                    let out = Text.Encoding.ASCII.GetBytes "OK"
+                    resp.OutputStream.Write(out, 0, out.Length)
+                    resp.OutputStream.Close()
+                    printfn "[%s] Sent response" (Utils.ToDateTimeString DateTime.Now)
+                }
+            )) |> ignore
+
+        printfn "Press any key to exit..."
+        Console.ReadKey true |> ignore
+        0
