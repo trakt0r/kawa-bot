c132e209083bae19710596988f0cc7d137e19500
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2016-12-23 01:51:08 +0100

    Allow handling different URLs

--- src/KawaBot.Server/Program.fs
+++ src/KawaBot.Server/Program.fs
@@ -2,24 +2,14 @@
 
 open System
 open System.Net
-open Utils
+open HealthController
 open Http
 
 module Program =
     [<EntryPoint>]
-    let main argv = 
-        printfn "[%s] Listening to the http://localhost:80/ started" (Utils.ToDateTimeString DateTime.Now)
-
-        HttpListener.Run("http://*:80/Health/", (fun req resp -> 
-                async {
-                    printfn "[%s] Received request to the /Health" (Utils.ToDateTimeString DateTime.Now)
-                    let out = Text.Encoding.ASCII.GetBytes "OK"
-                    resp.OutputStream.Write(out, 0, out.Length)
-                    resp.OutputStream.Close()
-                    printfn "[%s] Sent response" (Utils.ToDateTimeString DateTime.Now)
-                }
-            )) |> ignore
-
-        printfn "Press any key to exit..."
+    let main argv =
+        let listener = new HttpListener("http://*:80/")
+        listener.Register(HealthController.GetUrlHandler())
+        listener.Run()
         Console.ReadKey true |> ignore
         0
