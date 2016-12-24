8168d0e143a8d36a61882f3584ce0ed0d5fc9033
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2016-12-24 20:42:16 +0100

    [#10] Add HTTP feeder library

---
+++ src/KawaBot.Feeder/Feeder.fs
@@ -0,0 +1,21 @@
+﻿namespace KawaBot.Feeder
+
+open System
+open System.Net.Http
+
+type Feeder =
+    val private _client: HttpClient
+    val private _port: int
+
+    new(port: int) as this =
+        {
+            _client = new HttpClient()
+            _port = port
+        }
+        then
+            this._client.BaseAddress <- new Uri(String.Format("http://localhost:{0}", this._port))
+
+    member this.Send(message: string, ?method: HttpMethod): Async<HttpResponseMessage> =
+        new HttpRequestMessage(defaultArg method HttpMethod.Get, message)
+        |> this._client.SendAsync
+        |> Async.AwaitTask
