707449777ed7db22f16f2147a8fb112e1f2e3824
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2016-12-25 18:13:05 +0100

    Change the code to be more functional

---
+++ src/KawaBot.Server/Utils.fs
@@ -0,0 +1,10 @@
+﻿namespace KawaBot.Server
+
+open System.Collections.Generic
+
+[<AutoOpen>]
+module Utils =
+    let tryGet (dict: IReadOnlyDictionary<'K, 'V>) (key: 'K): option<'V> =
+        match dict.TryGetValue(key) with
+            | true, value -> Some(value)
+            | _ -> None
